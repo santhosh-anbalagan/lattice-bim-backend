49a9a0e9b72c5da43b4ac00791e17bc5c3d864e8
god <god@Godwins-MacBook-Pro.local>
2021-07-28 14:32:21 +0530

    Added: Files

---
+++ src/main/java/com/lattice/bimbackend/BimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lattice.bimbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lattice/bimbackend/BimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lattice.bimbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
